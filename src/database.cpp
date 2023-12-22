1842d35f03d51744152684599f17dfa30218ef04
Farhan Ahmed <farhan2004ahmed@gmail.com>
2023-12-23 01:32:00 +0530

    added Database.cpp

---
+++ src/database.cpp
@@ -0,0 +1,211 @@
+#ifndef database_cpp
+#define database_cpp
+
+#include "database.h"
+#include "string_helper.h"
+
+#include <iostream>
+
+using namespace std;
+
+const char DELIMETER = ';';
+
+Database ::Database() throw(IOError, MemoryError)
+{
+    try
+    {
+        this->vehicletable = new Table<Vehicle>("vechiles.txt");
+        this->userTable = new Table<User>("users.txt");
+        this->tripTable = new Table<Trip>("trips.txt");
+
+        this->fetchAllVechicles();
+        this->fetchAllUsers();
+        this->fetchAllTrips();
+    }
+    catch (...)
+    {
+        throw;
+    }
+}
+void Database ::fetchAllVechicles() throw(IOError, MemoryError)
+{
+    this->vehicleTable->fileStream.open(this->vehicleTable->filename);
+    if (!this->vehicleTable->fileStream)
+    {
+        throw IOError();
+    }
+
+    for (string line; getline(this->vehicleTable->fileStream, line);)
+    {
+        vector<string> components = split(line, DELIMETER);
+
+        auto recordId = stol(components[0]);
+        auto registrationNumber = components[1];
+        auto type = VechileType(stoi(components[2]));
+        auto seats = stoi(components[3]);
+        auto companyName = components[4];
+        auto pricePerKm = stod(components[5]);
+        auto PUCExpirrationDate = Date(components[6]);
+
+        Storable *record = new Vehicle(registrationNumber, type, seats, companyName, pricePerKm, PUCExpirrationDate, recordId);
+        if (!record)
+        {
+            throw MemoryError();
+        }
+        this->vehicleTable->record->push_back(record);
+    }
+
+    this->vehicleTable->fileStream.close();
+}
+void Database ::fetchAllUsers() throw(IOError, MemoryError)
+{
+    this->userTable->fileStream.open(this->userTable->fileName);
+
+    if (!this->userTable->fileStream)
+    {
+        throw IOError();
+    }
+    for (string line; getline(this->userTable->fileStream, line);)
+    {
+        vector<string> components = split(line, DELIMETER);
+        auto recordId = stoi(components[0]);
+        auto name = components[1];
+        auto contact = components[2];
+        auto email = components[3];
+
+        Storable *record = new User(name, contact, email, recordId);
+        if (!record)
+        {
+            throw MemoryError();
+        }
+        this->userTable->records->push_back(record);
+    }
+    this->userTable->filestream.close();
+}
+void Database ::fetchAllTrips() throw(IOError, MemoryError)
+{
+    this->tripTable->fileStream.open(this->tripTable->filename);
+    if (!this->tripTable->fileStream)
+    {
+        throw IOError();
+    }
+    for (string line; getline(this->tripTable->filestream, line);)
+    {
+        vector<string> components = split(line, DELIMETER);
+
+        try
+        {
+            auto recordId = stoi(components[0]);
+            auto vehiclePtr = this->vehicleTable->getReferenceOfRecordForId(stoi(components[1]));
+            auto userPtr = this->userTable->getReferenceOfRecordForId(stoi(components[2]));
+            auto startDate = Date(components[3]);
+            auto endDate = Date(components[4]);
+            auto startReading = stol(components[5]);
+            auto endReading = stol(components[6]);
+            auto fare = stod(components[7]);
+            auto isCompleted = components[8] == "0" ? false : true;
+            Storable *record = new Trip(vehiclePtr, userptr, startDate, endDate, recordId, startReading, endReading, fare, isCompleted);
+            if (!record)
+            {
+                throw MemoryError();
+            }
+            this->tripTable->records->push_back(record);
+        }
+        catch (...)
+        {
+            // ignore error
+        }
+    }
+    this->tripTable->fileStream.close();
+}
+
+const Vehicle *const Database ::getVehicle(string registrationNo)
+    const throw(NoSuchRecordError)
+{
+    for (auto &record : *this->vehicleTable->records)
+    {
+        Vehicle *vehicle = dynamic_cast<Vehicle *>(record);
+        if (vehicle)
+        {
+            if (vehicle->getRegistrationNumber() == registrationNo)
+            {
+                return vehicle;
+            }
+        }
+    }
+    throw NoSuchRecordError();
+}
+const User *const Database ::getUser(string contactNo) const throw(NoSuchRecordError)
+{
+    for (auto &record : *this->userTable->records)
+    {
+        User *user = dynamic_cast<User *>(record)
+        {
+            if (user)
+            {
+                if (user->getContact() == contactNo)
+                {
+                    return user;
+                }
+            }
+        }
+    }
+    throw NoSuchRecordError();
+}
+
+const vector<const Vehicle *> Database ::getVehicle(Date startDate, Date endDate, VehicleType type) const
+{
+    vector<const Vehicle *> vehicles = vector<const Vehicle *>();
+    for (auto &vrecord : *this->vehicleTable->records)
+    {
+        Vehicle vehicle = dynamic_cast<Vehicle *>(vrecord);
+        if (vehicle && vehicle->getVehicleType() == type)
+        {
+            /* Check if this vehicle is free between start Date and endDate */
+            bool tripFound = false;
+            for (auto &trecord : *this->tripTable->records)
+            {
+                Trip *trip = dynamic_cast<Trip *>(trecord);
+
+                if (
+                    trip & &!trip->isCompleted() &&
+                    trip->getVehicle().getRecordId() == vehicle->getRecordId() &&
+                    !(trip->getStartDate() >= endDate && trip->getEndDate() >= endDate) &&
+                    !(trip->getStartDate() <= startDate && trip -
+                                                               > getEndDate() <= startDate))
+                {
+                    tripFound = true;
+                    break;
+                }
+            }
+            if (!tripFound)
+            {
+                vehicles.push_back(vehicle);
+            }
+        }
+    }
+    return vehicles;
+}
+void Database ::cleanUp()
+{
+    delete this->vehicleTable;
+    delete this->userTable;
+    delete this->tripTable;
+}
+Database ::~Database()
+{
+    thid->cleanUp();
+}
+const Table<Vehicle> *const Database::getVehicleRef() const
+{
+    return this->vehicleTable;
+}
+const Table<User> *const Database ::getUserRef() const
+{
+    return this->userTable;
+}
+const Table<Trip> *const Database::getTripRef() const
+{
+    return this->tripTable;
+}
+#endif
