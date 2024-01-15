dc2f40fda942b09ddedaf2bce404a16afb2f2548
Farhan Ahmed <farhan2004ahmed@gmail.com>
2024-01-15 07:56:11 +0530

    menu design and admin func added

--- src/admin.cpp
+++ src/admin.cpp
@@ -1,9 +1,17 @@
 #include <iostream>
 #include <stdlib.h>
 #include <fstream>
+#include <string>
 #include "admin.h"
-#include"main_menu.h"
+#include "main_menu.h"
 using namespace std;
+
+/*function declaration*/
+int add_vehicle();
+void delete_vehicle();
+void display();
+
+/*function declaration*/
 
 void admin_menu()
 {
@@ -11,23 +19,88 @@ void admin_menu()
     cout << ("\n1.New Admin \n2.Add vehicles \n3.Delete Vehicles \n4.Display \n5.Main Menu\n");
     cout << ("\nEnter your choice:");
     cin >> choice;
-    if (choice == 2)
+    if (choice == 1)
+    {
+        printf("in new admin");
+    }
+    else if (choice == 2)
     {
         add_vehicle();
+        admin_menu();
+    }
+    else if (choice == 3)
+    {
+        delete_vehicle();
+        admin_menu();
+    }
+    else if (choice == 4)
+    {
+        display();
+        admin_menu();
     }
     else if (choice == 5)
     {
-       main_menu();
+        main_menu();
     }
 }
 int add_vehicle()
 {
-    ofstream out("Vehicle_data.txt");
-    string data;
-    cout<<"Enter the following datas:\n";
-    cout<<"1.Type of vehicle \n2.Number of Vehicle \n3.Model No \n4.Capacity \n5.Fare \n6.Tansmission\n";
-    cin>>data;
-    out<<data;
+    fstream out("vehicle_data.txt", ios::out | ios::app);
+    string type, trans, model, cap, fare, id, number;
+    cout << "Vehicle ID:";
+    cin >> id;
+    cout << "vehicle Type:";
+    cin >> type;
+    cout << "Vehicle Number:";
+    cin >> number;
+    cout << "Model No:";
+    cin >> model;
+    cout << "Capacity:";
+    cin >> cap;
+    cout << "Fare:";
+    cin >> fare;
+    cout << "Tansmission:";
+    cin >> trans;
+    out << id << "\t\t" << type << "\t\t" << number << "\t\t" << model << "\t\t" << cap << "\t\t" << fare << "\t\t" << trans << endl;
     out.close();
     return 0;
+}
+void display()
+{
+    int dis;
+    string read;
+    cout << "\n1.Display Data \n2.Display Available Vehicles\n";
+    cout << "Enter your choice:";
+    cin >> dis;
+    if (dis == 1)
+    {
+        cout << "\nVehicle Id"
+             << "\t"
+             << "Vehicle Type"
+             << "\t"
+             << "Vehicle Number"
+             << "\t"
+             << "Model Number"
+             << "\t"
+             << "Capacity"
+             << "\t"
+             << "Fare"
+             << "\t"
+             << "Transmission" << endl;
+        fstream in("vehicle_data.txt", ios::in);
+        while (in.eof() == 0)
+        {
+            getline(in, read);
+            cout << "\n"
+                 << read << endl;
+        }
+        in.close();
+    } // incomplete display func
+    else
+        printf("Wrong Choice!!!!");
+}
+
+void delete_vehicle()
+{
+    printf("in delete vechiles \n ");
 }
--- src/main_menu.cpp
+++ src/main_menu.cpp
@@ -1,9 +1,11 @@
-#include<iostream>
-#include<stdlib.h>
-#include<fstream>
-#include"main_menu.h"
-#include"user_menu.h"
-#include"admin.h"
+#include <iostream>
+#include <stdlib.h>
+#include <fstream>
+#include "main_menu.h"
+#include "user_menu.h"
+#include "admin.h"
+#include <cstring>
+#include <ctime>
 using namespace std;
 
 int main_menu()
@@ -11,16 +13,27 @@ int main_menu()
     int choice;
     do
     {
-        cout<<"\n \t\t Main Menu \t\t \n ";
-        cout<<"\n1.Admin\n2.User\n3.Exit\n";
-        cout<<"\n Enter your choice \n";
-        cin>>choice;
-        
+        cout << endl
+             << "\n\t   \t\t\t\t\t\t\t ----------------------------" << endl;
+        cout << endl
+             << "\n\t   \t\t\t\t\t\t\t\t\t   Main Menu   " << endl;
+        time_t now = time(nullptr);
+        string dt = ctime(&now);
+        cout << "\n\t   \t\t\t\t\t\t\t\t   Current Date:/time: " << dt << endl;
+        cout << endl
+             << "\n\t   \t\t\t\t\t\t\t ----------------------------" << endl;
+        cout << "\n\t   \t\t\t\t\t\t\t\t     1.  ADMIN   " << endl;
+        cout << "\n\t   \t\t\t\t\t\t\t\t     2.  USER   " << endl;
+        cout << "\n\t   \t\t\t\t\t\t\t\t     3.  EXIT   " << endl;
+
+        cout << "\n\t   \t\t\t\t\t\t\t\t   ENTER YOUR CHOICE :    ";
+        cin >> choice;
+
         if (choice == 1)
             admin_menu();
         else if (choice == 2)
             user_menu();
         else if (choice == 3)
-            exit(0);  
-    } while (choice>=3);   
+            exit(0);
+    } while (choice >= 3);
 }
